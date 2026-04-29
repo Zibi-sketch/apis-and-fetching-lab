11617318e02618d8d34a00bc0a95a0809d405237
ZizaKariuki <zizawabetti@gmail.com>
2026-04-29 21:57:19 +0300

    Trial Complete

--- index.js
+++ index.js
@@ -2,3 +2,81 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+//Step 1: Fetch Alerts for a State from the API
+
+const searchButton = document.getElementById("fetch-alerts");
+const inputField = document.getElementById("state-input");
+
+searchButton.addEventListener('click', (e) => {
+    let state = inputField.value.toUpperCase();
+    fetchWeatherAlerts(state);
+    inputField.value = "";
+})
+
+function fetchWeatherAlerts(state) {
+    fetch(`https://api.weather.gov/alerts/active?area=${state}`)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("State Not Found!");
+            }
+        })
+
+        .then(function (response) {
+            displayAlerts(response)
+            const errorContainer = document.getElementById("error-message");
+            errorContainer.textContent = "";
+        })
+
+        .catch(errorObject => {
+
+            const alertDisplay = document.getElementById("alerts-display");
+            if (alertDisplay) {
+                alertDisplay.innerHTML = "";
+                const errorContainer = document.getElementById("error-message")
+            };
+
+            if (errorContainer) {
+                errorContainer.style.display = "block";
+                errorContainer.innerHTML = "";
+                let errorMessage = document.createElement("p");
+
+                errorMessage.textContent = `Oops! Something went wrong: ${errorObject.message}`;
+
+                errorContainer.appendChild(errorMessage);
+            }
+
+            console.error("Error:", errorObject.message);
+        });
+}
+
+
+//Step 2: Display the Alerts on the Page
+
+function displayAlerts(data) {
+    const alertDisplay = document.getElementById("alerts-display");
+    alertDisplay.innerHTML = "";
+
+    //Displaying the summary message:
+    const title = data.title;
+    const alerts = data.features.length;
+    let summary = `"${title} : ${alerts}."`;
+
+    let sumMessage = document.createElement("p");
+    sumMessage.textContent = summary;
+    alertDisplay.appendChild(sumMessage);
+
+    //Displaying a list of alert headlines:
+    let list = document.createElement("ul");
+
+    data.features.forEach((line) => {
+        let headLine = document.createElement("li");
+        headLine.textContent = line.properties.headline;
+
+        list.appendChild(headLine);
+        alertDisplay.appendChild(list);
+    });
+}
+
+
